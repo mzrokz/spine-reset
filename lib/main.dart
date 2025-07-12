14b6c11ec3b66861132b2f846c1d3e1300edf0ef
Murtaza <mzrokz@live.com>
2025-07-12 21:40:05 +0530

    feat: created basic flutter app

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Blank Flutter App',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const BlankPage(),
+    );
+  }
+}
+
+class BlankPage extends StatelessWidget {
+  const BlankPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Blank Page'),
+      ),
+      body: Container(), // Empty container for blank page
+    );
+  }
+}
